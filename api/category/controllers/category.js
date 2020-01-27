07e023f04aad8b9e8a8b7c357247f3fe0ba8119d
Moses igbuku <captainoak@gmail.com>
2020-01-27 15:09:43 +0100

    added custom route annd action

--- api/category/controllers/category.js
+++ api/category/controllers/category.js
@@ -1,8 +1,28 @@
 'use strict';
+const { sanitizeEntity } = require('strapi-utils');
 
 /**
  * Read the documentation (https://strapi.io/documentation/3.0.0-beta.x/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+
+module.exports = {
+  /**
+   * Retrieve records.
+   *
+   * @return {Array}
+   */
+  async zenge(ctx) {
+    let entities;
+    if (ctx.query._q) {
+      entities = await strapi.services.restaurant.search(ctx.query);
+    } else {
+      entities = await strapi.services.restaurant.find(ctx.query);
+    }
+
+    return entities.map(entity =>
+      sanitizeEntity(entity, { model: strapi.models.restaurant })
+    );
+  },
+};
